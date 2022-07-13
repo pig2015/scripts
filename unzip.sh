d373affaa46b7f2bd11f849bc3f3999ec06db756
Jay Yang <jayyange@amazon.com>
2022-07-13 19:32:39 +0800

    unzip with folder

---
+++ unzip.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+for f in "$@"
+do
+    if [[ ! -f $f ]]; then
+        continue
+    fi
+
+    if [[ $f == *".tar" ]]; then
+        tar xvf $f
+    elif [[ $f == *".tar.gz" ]]; then
+        tar zxvf $f
+    elif [[ $f == *".tgz" ]]; then
+        tar zxvf $f
+    elif [[ $f == *".gz" ]]; then
+        gunzip $f
+    elif [[ $f == *".zip" ]]; then
+        d=${f::-4}
+	mkdir $d
+        unzip $f -d $d
+    elif [[ $f == *".rar" ]]; then
+        rar e $f
+    elif [[ $f == *".bz2" ]]; then
+	bunzip $f
+    else
+        echo "Error: unidentified compression type: $f"
+    fi
+done
