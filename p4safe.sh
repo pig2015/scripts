2b99feca38501800e4e7b02496ec63533ec919f8
Jay Yang <jayyan@rno2-container-xterm-006.prd.it.nvidia.com>
2025-02-02 20:06:26 -0800

    stable p4safe for recur query

--- p4safe.sh
+++ p4safe.sh
@@ -1,8 +1,48 @@
 #!/bin/bash
 
 
-p4 where &>/dev/null
-if [ $? -ne 0 ]; then
+has_p4client() {
+    config_path=$1/.p4config
+    if [ ! -f $config_path ]; then
+        echo 0
+        return
+    fi
+    match=$(grep "P4CLIENT=*"  $config_path)
+    if [ "$match" == "" ]; then
+        echo 0
+        return
+    fi
+    echo 1
+}
+
+
+is_p4ws() {
+    dir=$1
+    for _ in {1..100};
+    do
+        if [[ "$dir" == "$HOME" ]] || \
+           [[ "$dir" == "/home" ]] || \
+           [[ "$dir" == "/" ]]; then
+            echo 0
+            return
+        fi
+        res=$(has_p4client $dir)
+        if [ "$res" == "1" ]; then
+            echo 1
+            return
+        fi
+        if [ -L $dir ]; then
+            echo 0
+            return
+        fi
+        dir=$(dirname $dir)
+    done
+}
+
+
+# p4 where 1> /dev/null 2> /dev/null  # not supported under nv bash env
+res=$(is_p4ws $PWD)
+if [ "$res" -eq "0" ]; then
     echo "Invalid workdir, not a p4 workspace."
     exit 1
 fi
