343cfcd13ea7a6d1015738d8486a1c30320338a6
Jay Yang <jayyan@rno2-container-xterm-006.prd.it.nvidia.com>
2025-06-26 17:54:55 -0700

    fix searchf args forwarding issue

--- searchf.sh
+++ searchf.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ $# == 1 ]]; then
-    find . -name $1
+    find . -name "$1"
 elif [[ $# == 2 ]];then
-    find $2 -name $1
+    find "$2" -name "$1"
 else
     echo "Error. Usage: <cmd> '<filename_pattern>' [<folder>]"
 fi
