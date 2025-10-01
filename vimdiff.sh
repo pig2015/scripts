ee156f21df56a413cb8b8d9be4e052ece3bd2f03
Jay Yang <jayyan@rno2-container-xterm-006.prd.it.nvidia.com>
2025-10-01 01:23:10 -0700

    better vimdiff opt to make :qa as :q

--- vimdiff.sh
+++ vimdiff.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 VIM_BIN=vimdiff
-exec $VIM_BIN -c 'set diffopt+=iwhite' -c 'set number' "$@"
+exec $VIM_BIN \
+    -c 'set diffopt+=iwhite' \
+    -c 'set number' \
+    -c 'autocmd VimEnter * if &diff | cnoreabbrev q qa | endif' \
+    "$@"
 
